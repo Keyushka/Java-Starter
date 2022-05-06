5e5dd4e14c681a55f7a46b41f0ff2d67c200bf12
IKononenko <iryna.kononenko@microfocus.com>
2022-05-06 19:54:34 +0300

    HM7 - task5

--- src/javaStarter/homework/hm7/Task5_Calculator.java
+++ src/javaStarter/homework/hm7/Task5_Calculator.java
@@ -1,7 +1,20 @@
 package javaStarter.homework.hm7;
 
+import java.util.Scanner;
+
 public class Task5_Calculator {
     public static void main(String[] args) {
-
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Введите 1е целое число: ");
+        int number1 = sc.nextInt();
+        System.out.println("Введите 2е целое число: ");
+        int number2 = sc.nextInt();
+        System.out.println("Введите 3е целое число: ");
+        int number3 = sc.nextInt();
+        calculate(number1, number2, number3);
+    }
+    public static void calculate(int val1, int val2, int val3) {
+        double average = (double) (val1 + val2 + val3) / 3;
+        System.out.println("Среднее арифметическое трех целочисленных значений = " + average);
     }
 }
