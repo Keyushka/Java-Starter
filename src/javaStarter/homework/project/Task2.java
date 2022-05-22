1a212c21aad66eb42a945e003ece7579f3589d3e
Iryna Kononenko <keyushka@gmail.com>
2022-05-23 00:35:09 +0300

    course project "Java Starter" - task 2,3

--- src/javaStarter/homework/project/Task2.java
+++ src/javaStarter/homework/project/Task2.java
@@ -1,10 +1,26 @@
 package javaStarter.homework.project;
 
+import java.util.Scanner;
+
 // Напишите метод, который принимает число,
 // которое не содержит нулей и возвращает цифры в обратном порядке.
 // Например, на вход: 789 на выход: 987
 public class Task2 {
     public static void main(String[] args) {
+        System.out.println("Напишите число, которое не содержит нулей: ");
+        Scanner sc = new Scanner(System.in);
+        int num = sc.nextInt();
+        System.out.println("Цифры числа справа налево: ");
+        System.out.println(fromRightToLeft(num));
+    }
 
+    public static int fromRightToLeft (int num) {
+        if (num < 10) {
+            return num;
+        }
+        else {
+            System.out.print(num % 10);
+            return fromRightToLeft(num / 10);
+        }
     }
 }
--- src/javaStarter/homework/project/Task3.java
+++ src/javaStarter/homework/project/Task3.java
@@ -1,9 +1,26 @@
 package javaStarter.homework.project;
 
+import java.util.Scanner;
+
 // Создайте программу, которая выводит в консоль число имени, например,
 // на вход Roman = R(18) + О(15) + M(13) + A(1) + N(14) = 61 на выход 61.
 public class Task3 {
     public static void main(String[] args) {
-
+        System.out.println("Введите любое имя: ");
+        Scanner in = new Scanner(System.in);
+        String name = in.nextLine();
+        char[] nameArr = name.toCharArray();
+        System.out.println("Char array:");
+        int sumNumbers = 0;
+        for (int i = 0; i < nameArr.length; i++) {
+            int letter = name.charAt(i);
+            if (Character.isLowerCase(letter)) {
+                sumNumbers += letter-96;
+            } else if (Character.isUpperCase(letter)) {
+                sumNumbers += letter-64;
+            }
+        }
+        System.out.println("Число введенного имени = " + sumNumbers);
+        in.close();
     }
 }
